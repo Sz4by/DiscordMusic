ef76e87e3a83c5dd5cffaf52a96b660b66cd2e85
Sz4by <112946552+Sz4by@users.noreply.github.com>
2024-07-18 16:17:52 +0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -112,7 +112,7 @@ const numberEmojis = [
 
 const slashSettings = {
   global: false,
-  guildIDS: ["903532162236694539"], // for slash command
+  guildIDS: ["1158021308412600373"], // for slash command
 };
 
 module.exports = {
